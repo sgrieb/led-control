435d0de31227c8f23dfbbb24c61534c9ba0f9a7b
Steve <steve@Macs-MacBook-Pro-5.local>
2017-11-12 01:18:50 -0500

    img switch

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser')
 var Led = require('./server/led');
+const fs = require('fs');
 
 // front end serve
 app.use(express.static('html'));
 app.use('/js', express.static('js'));
 app.use('/css', express.static('css'));
 
-var myLed = new Led('');
+app.use(bodyParser.json());
+
+var myLed = new Led('xm.gif');
 
 // backend
 app.post('/start', (req, res) => {
@@ -22,6 +26,28 @@ app.post('/stop', (req, res) => {
   console.log('stop');
   myLed.stop();
 
+  res.sendStatus(200);
+});
+
+app.get('/img', (req, res) => {
+
+  var folder = '/Users/steve/Projects/led-control';
+  //var folder = '/app/led/rpi-rgb-led-matrix/utils/img'
+
+  var folders = fs.readdirSync(folder);
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(folders));
+});
+
+app.post('/img', (req, res) => {
+  
+  var folder = '/Users/steve/Projects/led-control';
+  //var folder = '/app/led/rpi-rgb-led-matrix/utils/img'
+
+  myLed = new Led(req.body.name);
+  myLed.start();
+  
   res.sendStatus(200);
 });
 
--- js/main.js
+++ js/main.js
@@ -1,22 +1,56 @@
 
-$('.btn-danger').click(function() {
+
+
+$(function() {
+    // setup handlers
+    $('.btn-danger').click(function() {
+        $.ajax({
+            type: "POST",
+            url: '/stop',
+            data: {},
+            success: function() {
+                console.log('stop success');
+            }
+        });
+    });
+    
+    $('.btn-success').click(function() {
+        $.ajax({
+            type: "POST",
+            url: '/start',
+            data: {},
+            success: function() {
+                console.log('start success');
+            }
+        });
+    });
+
+    // get images
     $.ajax({
-        type: "POST",
-        url: '/stop',
-        data: {},
-        success: function() {
-            console.log('stop success');
+        type: "GET",
+        url: '/img',
+        success: function(result) {
+            console.log('get img success');
+            if (result) {
+                result.forEach(function(img) {
+                    $('tbody').append('<tr><td>'+ img +'</td><td><button type="button" class="btn btn-success btn-set" data-img="'+ img +'">Set</button></td></tr>');
+                });
+
+                $('.btn-set').click(function() {
+                    var name = $(this).data('img');
+
+                    $.ajax({
+                        type: "POST",
+                        url: '/img',
+                        contentType: 'application/json', 
+                        data: JSON.stringify({name: name}),
+                        success: function() {
+                            console.log('set success');
+                        }
+                    });
+                });
+            }
         }
     });
 });
 
-$('.btn-success').click(function() {
-    $.ajax({
-        type: "POST",
-        url: '/start',
-        data: {},
-        success: function() {
-            console.log('start success');
-        }
-    });
-});
