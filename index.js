e11d2ccbab5456569ec1419387a7028f99fda2fc
Steve <steve@Macs-MacBook-Pro-5.local>
2017-11-11 19:25:30 -0500

    version

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
-
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const child_process = require('child_process');
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-const { exec } = require('child_process');
-exec('cd /app/led/rpi-rgb-led-matrix && sudo examples-api-use/demo -D0', (error, stdout, stderr) => {
+child_process.exec('cd /app/led/rpi-rgb-led-matrix && sudo examples-api-use/demo -D0', (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
