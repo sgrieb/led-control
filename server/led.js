502ae5839b2c1fcca022c6a81fd3f585b4bf2f0d
Steve <steve@Macs-MacBook-Pro-5.local>
2017-11-11 21:43:42 -0500

    stop

--- server/led.js
+++ server/led.js
@@ -12,21 +12,23 @@ Led.prototype.start = function() {
 
     //this.cmd = child_process.spawn('ls');
     //this.cmd = child_process.spawn('cd /app/led/rpi-rgb-led-matrix && sudo examples-api-use/demo -D0');
-    this.cmd = child_process.exec('cd /app/led/rpi-rgb-led-matrix && nohup sudo examples-api-use/demo -D0 </dev/null >myprogram.log & ', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
+    this.cmd = child_process.exec('cd /app/led/rpi-rgb-led-matrix && nohup sudo examples-api-use/demo -D0 </dev/null >myprogram.log & ', onCmdFinish);
 };
 
 Led.prototype.stop = function() {
     if (this.cmd) {
-        this.cmd.kill();
+        this.cmd = child_process.exec('killall demo', onCmdFinish);
         console.log('process killed');
     }
 };
+
+function onCmdFinish(error, stdout, stderr) {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+}
 
 module.exports = Led;
