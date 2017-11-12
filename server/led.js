797ede4574978fe45b180bf33244a3679d42c1f6
Steve <steve@Macs-MacBook-Pro-5.local>
2017-11-11 21:17:04 -0500

    exec

--- server/led.js
+++ server/led.js
@@ -11,7 +11,15 @@ Led.prototype.start = function() {
     }
 
     //this.cmd = child_process.spawn('ls');
-    this.cmd = child_process.spawn('cd /app/led/rpi-rgb-led-matrix && sudo examples-api-use/demo -D0');
+    //this.cmd = child_process.spawn('cd /app/led/rpi-rgb-led-matrix && sudo examples-api-use/demo -D0');
+    this.cmd = child_process.exec('cd /app/led/rpi-rgb-led-matrix && sudo examples-api-use/demo -D0', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
 };
 
 Led.prototype.stop = function() {
